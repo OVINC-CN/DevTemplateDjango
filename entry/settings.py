e4691aab80f6082f2edc4fb72a326fa9145dac55
orenzhang <orenzhang@ovinc.cn>
2023-09-24 22:30:26 +0800

    feat: enable timezone

--- entry/settings.py
+++ entry/settings.py
@@ -132,7 +132,7 @@ LANGUAGE_CODE = os.getenv("DEFAULT_LANGUAGE", "zh-hans")
 TIME_ZONE = os.getenv("DEFAULT_TIME_ZONE", "Asia/Shanghai")
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
+USE_TZ = True
 LANGUAGES = (("zh-hans", "中文简体"), ("en", "English"))
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
