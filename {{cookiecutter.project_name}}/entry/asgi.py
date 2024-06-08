88dbc860cb14d99e62f5c2aa30f7d823b8a85fe8
Oren Zhang <orenzhang@ovinc.cn>
2024-06-08 20:50:52 +0800

    feat: add asgi ot instrument

--- {{cookiecutter.project_name}}/entry/asgi.py
+++ {{cookiecutter.project_name}}/entry/asgi.py
@@ -2,11 +2,12 @@ import os
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entry.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": OpenTelemetryMiddleware(get_asgi_application()),
     }
 )
