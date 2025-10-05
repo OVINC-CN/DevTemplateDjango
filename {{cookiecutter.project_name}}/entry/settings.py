841a1bc85c7f2d66306150e07a65128c2658b309
U8F69 <orenzhang@ovinc.cn>
2025-10-05 19:59:11 +0800

    chore(settings): add ovinc client to apps

--- {{cookiecutter.project_name}}/entry/settings.py
+++ {{cookiecutter.project_name}}/entry/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "sslserver",
+    "ovinc_client",
     "ovinc_client.account",
     "ovinc_client.trace",
     "apps.cel",
